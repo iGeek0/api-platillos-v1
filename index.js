86ff22d12f0d9951866387bd0e8a34b6f3636fa6
iGeek <jesus.cardenas.d@gmail.com>
2023-05-11 19:47:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(express.json());
 
 //minibase de datos de platillos
 
-let platillos = [{
+let platillos = [
+{
     "id": 1,
     "nombre": "Tacos al pastor",
     "precio": 10,
@@ -80,6 +81,32 @@ app.delete("/platillos/:id",(req, res)=>{
     }
 
 });
+
+app.put("/platillos/:id",(req, res)=>{
+    const idBuscar = parseInt(req.params.id);
+
+    let platilloResultado = platillos.find((platillo)=> { return platillo.id === idBuscar });
+
+    if (platilloResultado) {
+        // realizar tarea correspondiente(update)
+        platilloResultado.nombre = req.body.nombre;
+        platilloResultado.precio = req.body.precio;
+        platilloResultado.descripcion = req.body.descripcion;
+        res.json({
+            message: "El platillo fue editado",
+            data: platilloResultado
+        })
+    } else {
+        // en caso de no existir el platillo que procede.....
+        res.json({
+            message: "El id enviado no pertenece a un platillo",
+            data: null
+        })
+    }
+
+});
+
+
 
 
 app.listen(process.env.PORT, ()=>{
